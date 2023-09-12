42be6136f042c9cf9532aa7543bc9fa75a64b2f6
Jose Ricardo Moreira Junior <josericardomoeira96@gmail.com>
2023-09-12 12:44:04 -0300

    .

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,7 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println(1+1+1+"1");
+        System.out.println(1+"1"+1+1);
+        System.out.println("1"+(1+1+1));
+    }
+}

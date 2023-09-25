88ea86f54744008a88deb5c2475d0595628c914b
José Ricardo Moreira <josericardomoreira96@gmail.com>
2023-09-25 16:57:39 -0300

    Create AparelhoTelefonico.java

---
+++ Desafio-POO/AparelhoTelefonico.java
@@ -0,0 +1,24 @@
+public interface AparelhoTelefonico {
+    void fazerChamada();
+    void atenderChamada();
+    void iniciarGravacaoVoz();
+}
+
+// AparelhoTelefonicoImpl.java
+
+public class AparelhoTelefonicoImpl implements AparelhoTelefonico {
+    @Override
+    public void fazerChamada() {
+        // Implementação para fazer uma chamada
+    }
+
+    @Override
+    public void atenderChamada() {
+        // Implementação para atender uma chamada
+    }
+
+    @Override
+    public void iniciarGravacaoVoz() {
+        // Implementação para iniciar a gravação de voz
+    }
+}

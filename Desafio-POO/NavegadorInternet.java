780747e074e944db7565ea3d80dcd16cd1caaf0c
José Ricardo Moreira <josericardomoreira96@gmail.com>
2023-09-25 16:58:12 -0300

    Create NavegadorInternet.java

---
+++ Desafio-POO/NavegadorInternet.java
@@ -0,0 +1,24 @@
+public interface NavegadorInternet {
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
+
+// NavegadorInternetImpl.java
+
+public class NavegadorInternetImpl implements NavegadorInternet {
+    @Override
+    public void exibirPagina() {
+        // Implementação para exibir uma página da web
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Implementação para adicionar uma nova aba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Implementação para atualizar uma página
+    }
+}
